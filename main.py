9a395335a100bd55799ed9bd8821174d778e79be
k1rill-dev <kirillbelokobylsky@yandex.ru>
2023-02-17 12:29:31 +0300

    add post/get endpoints

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+import io
+import PIL.Image as Image
 from fastapi import FastAPI
 from scan_qr import detect_qr
 from pydantic import BaseModel as PydanticBaseModel
+import base64
+
 
 class BaseModel(PydanticBaseModel):
     class Config:
         arbitrary_types_allowed = True
 
 
-class Image(BaseModel):
-    file: bytearray
+class Qr(BaseModel):
+    file: str
 
 
 app = FastAPI()
 
 
 @app.post("/check_qr")
-async def root(img: Image):
-
-    value: str = detect_qr(img.file)
+async def check_qr_post(qr: Qr):
+    byte_decoded = base64.b64decode(qr.file)
+    image = Image.open(io.BytesIO(byte_decoded))
+    value: str = detect_qr(image)
 
     return {"success": True,
             "data": value}
 
+
+@app.get("/check_qr")
+async def check_qr_get():
+    return {"success": True}
--- scan_qr.py
+++ scan_qr.py
@@ -2,27 +2,11 @@ from PIL import Image
 from pyzbar.pyzbar import decode
 
 
+def detect_qr(image: Image) -> str:
+    """Распознает QR-код и возвращает его содержимого"""
 
-def detect_qr(filename: str) -> str:
     try:
-        result = decode(Image.open(filename))
+        result = decode(image)
         return result[0].data
     except:
         return "Не найдены qr коды на фото!"
-
-
-# print(detect_qr('sticker.png'))
-import os
-import io
-import PIL.Image as Image
-
-from array import array
-
-def readimage(path):
-    # count = os.stat(path).st_size / 2
-    with open(path, "rb") as f:
-        return bytearray(f.read())
-
-bytes = readimage("D:\python_projects\qr\qr.jpg")
-image = Image.open(io.BytesIO(bytes))
-image.save('a.png')
